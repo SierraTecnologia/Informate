392273617e34c9ac848dac87165acd3eabae429c
Ricardo Rebello Sierra <contato@ricardosierra.com.br>
2019-11-24 00:02:40 -0300

    Provider

--- src/InformateProvider.php
+++ src/InformateProvider.php
@@ -8,26 +8,34 @@ use Illuminate\Support\ServiceProvider;
 
 class InformateProvider extends ServiceProvider
 {
+    public static $providers = [
+        // \Informate\Providers\InformateEventServiceProvider::class,
+        // \Informate\Providers\InformateServiceProvider::class,
+        // \Informate\Providers\InformateRouteProvider::class,
+
+        // \Audit\AuditProvider::class,
+        // \Casa\CasaProvider::class,
+    ];
+
     /**
      * Alias the services in the boot.
      */
     public function boot()
     {
-        $this->publishes([
-            __DIR__.'/Publishes/resources/tools' => base_path('resources/tools'),
-            __DIR__.'/Publishes/app/Services' => app_path('Services'),
-            __DIR__.'/Publishes/public/js' => base_path('public/js'),
-            __DIR__.'/Publishes/public/css' => base_path('public/css'),
-            __DIR__.'/Publishes/public/img' => base_path('public/img'),
-            __DIR__.'/Publishes/config' => base_path('config'),
-            __DIR__.'/Publishes/routes' => base_path('routes'),
-            __DIR__.'/Publishes/app/Controllers' => app_path('Http/Controllers/Informate'),
-        ]);
+        // $this->publishes([
+        //     __DIR__.'/Publishes/resources/tools' => base_path('resources/tools'),
+        //     __DIR__.'/Publishes/app/Services' => app_path('Services'),
+        //     __DIR__.'/Publishes/public/js' => base_path('public/js'),
+        //     __DIR__.'/Publishes/public/css' => base_path('public/css'),
+        //     __DIR__.'/Publishes/public/img' => base_path('public/img'),
+        //     __DIR__.'/Publishes/config' => base_path('config'),
+        //     __DIR__.'/Publishes/routes' => base_path('routes'),
+        //     __DIR__.'/Publishes/app/Controllers' => app_path('Http/Controllers/Informate'),
+        // ]);
 
-        $this->publishes([
-            __DIR__.'../resources/views' => base_path('resources/views/vendor/informate'),
-        ], 'SierraTecnologia Informate');
-       
+        // $this->publishes([
+        //     __DIR__.'../resources/views' => base_path('resources/views/vendor/Informate'),
+        // ], 'SierraTecnologia Informate');
     }
 
     /**
@@ -37,13 +45,19 @@ class InformateProvider extends ServiceProvider
     {
         $this->setProviders();
 
-        // View namespace
-        $this->loadViewsFrom(__DIR__.'/Views', 'informate');
+        // // View namespace
+        // $this->loadViewsFrom(__DIR__.'/Views', 'Informate');
+
+        // if (is_dir(base_path('resources/Informate'))) {
+        //     $this->app->view->addNamespace('Informate-frontend', base_path('resources/Informate'));
+        // } else {
+        //     $this->app->view->addNamespace('Informate-frontend', __DIR__.'/Publishes/resources/Informate');
+        // }
 
         $this->loadMigrationsFrom(__DIR__.'/Migrations');
 
-        // Configs
-        $this->app->config->set('informate.modules.informate', include(__DIR__.'/config.php'));
+        // // Configs
+        // $this->app->config->set('Informate.modules.Informate', include(__DIR__.'/config.php'));
 
         /*
         |--------------------------------------------------------------------------
@@ -56,9 +70,9 @@ class InformateProvider extends ServiceProvider
 
     protected function setProviders()
     {
-        $this->app->register(\Informate\Providers\InformateEventServiceProvider::class);
-        $this->app->register(\Informate\Providers\InformateServiceProvider::class);
-        $this->app->register(\Informate\Providers\InformateRouteProvider::class);
+        collection(self::$providers)->map(function ($provider) {
+            $this->app->register($provider);
+        })
 
     }
 
